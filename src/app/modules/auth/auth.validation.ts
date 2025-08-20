ad3e07e750c05649df3ae02d3053b21b6bf24124
TayeburRahman <tayebrayhan101+@gmail.com>
2025-08-20 18:59:32 +0600

    updates all

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -3,19 +3,19 @@ import { z } from "zod";
 // --- CREATE ACCOUNT VALIDATION ---
 const create = z.object({
   body: z.object({
-    firstName: z
-      .string({ required_error: "First Name is required" })
+    name: z
+      .string({ required_error: "Name is required" })
       .min(1, "First name cannot be empty"),
-    lastName: z
-      .string({ required_error: "Last Name is required" })
-      .min(1, "Last name cannot be empty"),
     email: z
       .string({ required_error: "Email is required" })
       .email("Invalid email format"),
     password: z
       .string({ required_error: "Password is required" })
       .min(6, "Password must be at least 6 characters long"),
-    phoneNumber: z.string().optional(),
+    confirmPassword: z
+      .string({ required_error: "Password is required" })
+      .min(6, "Password must be at least 6 characters long"),
+    phone_number: z.string().optional(),
     address: z.string().optional(),
     role: z.enum(["CUSTOMER", "ADMIN", "AGENT", "SUPER_ADMIN"]),
   }),
@@ -25,18 +25,16 @@ const create = z.object({
 const updateUserZodSchema = z.object({
   body: z.object({
     name: z
-      .object({
-        firstName: z.string().optional(),
-        lastName: z.string().optional(),
-      })
+      .string({ required_error: "Name is required" })
+      .min(1, "First name cannot be empty")
       .optional(),
     email: z.string().email("Invalid email format").optional(),
-    phoneNumber: z.string().optional(),
+    phone_number: z.string().optional(),
     password: z.string().min(6, "Password must be at least 6 characters").optional(),
     address: z.string().optional(),
     profile_image: z.string().optional(),
     role: z.enum(["CUSTOMER", "ADMIN", "AGENT", "SUPER_ADMIN"]).optional(),
-    date_of_birth: z.string().optional(), // can parse later to Date
+    date_of_birth: z.string().optional(),
   }),
 });
 
